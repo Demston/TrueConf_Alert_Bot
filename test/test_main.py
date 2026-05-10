139c4df31870f1c9baca031fac9442af2362ed41
Demston <demston89@gmail.com>
2026-05-10 17:46:09 +0300

    Update

---
+++ test/test_main.py
@@ -0,0 +1,26 @@
+import pytest
+from fastapi.testclient import TestClient
+from main_app import app
+
+client = TestClient(app)
+
+
+@pytest.fixture
+def grafana_payload():
+    return {
+        "title": "Critical Erros",
+        "state": "alerting",
+        "message": "Metric Value: 5"
+    }
+
+
+def test_grafana_webhook_success(grafana_payload):
+    """Check API work / Проверяем работу API"""
+    response = client.post("/webhook/grafana", json=grafana_payload)
+    assert response.status_code == 200
+
+
+def test_webhook_invalid_json(grafana_payload):
+    """Send json / Отправляем json"""
+    response = client.post("/webhook/grafana", json=grafana_payload)
+    assert response.status_code != 500
