99957efeb65e625677454e27df929d6e192eedc3
Demston <demston89@mail.ru>
2026-05-26 01:00:59 +0300

    Update online flag

--- v2_enterprise/handler/broker_handlers/check_alerts.py
+++ v2_enterprise/handler/broker_handlers/check_alerts.py
@@ -9,8 +9,9 @@ from settings import setting
 from core.logger import logger
 
 
-# Словарь для таймингов (антиспам)
+# antispam dict
 ALERTS_LAST_SEND_TIME = {}
+
 
 async def register(state: AppState):
     if getattr(state, "_check_alerts_broker_registered", False):
@@ -20,51 +21,51 @@ async def register(state: AppState):
     async def task_handler(msg):
 
         current_time = datetime.now().time()
-        if not (time(9, 0) <= current_time <= time(18, 0)): # Рабочее ли время (9-18ч.)
+        if not (time(9, 0) <= current_time <= time(18, 0)):     # work time (9-18)
             return
 
         timestamp = datetime.now().strftime('%H:%M')
 
         # if not state.is_ready:
         #     return
-        # если бот подключен — принудительно оживляем флаг для системы
+        # if bot online - repair's flag for system
         state.is_ready = True
 
-        # msg = json.loads(msg) # не нужно, уже - словарь
+        # msg = json.loads(msg)
         full_message = ''
         alerts = msg.get('alerts', [])
         for alert in alerts:
             # status = alert.get('status')
             fingerprint = alert.get('fingerprint')
             alert_name = alert.get('labels', {}).get('alertname', 'Алерт')
-            values = alert.get('values') or {} # Вытаскиваем только имя и цифру
-            val = values.get('B', values.get('A', 0)) # Пробуем взять B (Reduce) или A (SQL), если пусто — 0
+            values = alert.get('values') or {}  # get num only
+            val = values.get('B', values.get('A', 0))  # get B (Reduce) or A (SQL), 0 if empty
             if val == 0:
-                continue  # Пропускаем алёрт нулём и идем дальше по циклу
+                continue  # skip 0-alert
 
-            # фильтр от спама
+            # antispam filter
             now_ts = datetime.now().timestamp()
-            # Если этот алёрт уже уходил меньше заданного интервала назад (указываем в секундах) — скипаем его повтор
+
             if fingerprint in ALERTS_LAST_SEND_TIME and (now_ts - ALERTS_LAST_SEND_TIME[fingerprint] < 120):
                 continue
-            ALERTS_LAST_SEND_TIME[fingerprint] = now_ts  # запоминаем время отправки
+            ALERTS_LAST_SEND_TIME[fingerprint] = now_ts  # remember send time
 
-            # Формируем строку с алёртом
+            # final string with alert
             full_message = f"{timestamp} {alert_name}:  {val}"
 
-        if not full_message:  # Периодически прилетают пустые фантомные сообщения
-            return  # Если все алерты пустые, дальше не идем
+        if not full_message:  # if empty
+            return
 
         duties_list = get_today_duty(source="db")  # excel or db
         if not duties_list:
-            return  # Безопасно завершим, чтобы не плодить очередь
+            return
 
         # with open(setting.CONTACTS_NAMES, 'r', encoding='utf-8') as f:
-        #     contacts_dict = json.load(f)       # старый вариант
+        #     contacts_dict = json.load(f)       # old
 
         duties_list_id = []
         for dut in duties_list:
-            # Если None или пустота — скипаем
+            # skip if none
             if not dut:
                 continue
             with get_db() as db:
@@ -73,7 +74,7 @@ async def register(state: AppState):
                     duties_list_id.append(str(chat_id))
 
         # for chat_id, user_name in contacts_dict.items():
-        #   if user_name in duties_list:  # старый вариант
+        #   if user_name in duties_list:  # old
 
         for chat_id in duties_list_id:
             try:
